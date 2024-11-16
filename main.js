15f121afac175fae7688177e0f3cbd4a25b5d19b
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-16 15:28:14 +0530

    fileSystem

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function printFileContents(filePath) {
-  // TODO: Use fs.readFile to read the file contents
-  fs.readFile(filePath, 'utf-8', (err, data) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      console.log(err)
-      return
+      if (err.code === 'ENOENT') {
+        console.error(`Error: Column '${filePath}' not found in the CSV.`);
+      } else if (err.code === 'EACCES') {
+        console.error(`Error: Permission denied to access the file at '${filePath}'.`);
+      } else {
+        console.error(`Error: An error occurred while reading the file: ${err.message}`);
+      }
+      return;
     }
-    console.log(data.toString())
-  })
+    console.log(data);
+  });
 }
 
-// TODO: Call printFileContents with the command-line argument
-const filePath = process.argv[2]
+// Retrieve the command-line argument
+const filePath = process.argv[2];
+
 if (!filePath) {
-  console.error(
-    'Error: No file path provided. Please specify a file path as an argument.'
-  )
-  process.exit(1)
+  console.error("Error: No file path provided. Please specify a file path as an argument.");
+  process.exit(1);
 }
 
-printFileContents(filePath)
+// Call the function with the provided argument
+printFileContents(filePath);
