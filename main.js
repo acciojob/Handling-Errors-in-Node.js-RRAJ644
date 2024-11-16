ef9c2efcb94328bc4ef648c26dffdc3cf5564165
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-16 14:46:25 +0530

    readfile

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-const fs = require('fs');
+const fs = require('fs')
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log(data.toString())
+  })
 }
 
 // TODO: Call printFileContents with the command-line argument
+const filePath = process.argv[2]
+if (!filePath) {
+  console.error(
+    'Error: No file path provided. Please specify a file path as an argument.'
+  )
+  process.exit(1)
+}
+
+printFileContents(filePath)
